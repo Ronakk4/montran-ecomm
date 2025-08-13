b4db83818916b63c7bbcf027dc98fab283b04368
Harsh Chheda <chhedaharsh06@gmail.com>
2025-08-13 10:35:06 +0530

    service interface and impl initialized

---
+++ src/main/java/com/capstone/service/impl/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package com.capstone.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.capstone.service.Order;
+import com.capstone.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+	@Override
+	public List<Order> getAllOrders(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order getOrder(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOrder(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveOrder(Order o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/capstone/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.capstone.service.impl;
+import org.springframework.stereotype.Service;
+
+import com.capstone.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+}
