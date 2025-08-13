d34aa3892906657944870b74b792804cbe34b958
Harsh Chheda <chhedaharsh06@gmail.com>
2025-08-13 10:59:21 +0530

    User service and impl added

--- src/main/java/com/capstone/service/UserService.java
+++ src/main/java/com/capstone/service/UserService.java
@@ -1,5 +1,10 @@
 package com.capstone.service;
 
-public interface UserService {
+import com.capstone.model.User;
 
+public interface UserService {
+	User findUser(long id);
+	void updateUser(User u);
+	void loginUser(User u);
+	void registerUser(User u);
 }
--- src/main/java/com/capstone/service/impl/UserServiceImpl.java
+++ src/main/java/com/capstone/service/impl/UserServiceImpl.java
@@ -1,9 +1,46 @@
 package com.capstone.service.impl;
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.capstone.model.User;
 import com.capstone.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Override
+	@Transactional
+	public User findUser(long id) {
+		// TODO Auto-generated method stub
+		return userDao.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public void registerUser(User u) {
+		// TODO Auto-generated method stub
+		userDao.save(u);
+		
+	}
+
+	@Override
+	public void updateUser(User u) {
+		// TODO Auto-generated method stub
+		userDao.update(u);
+		
+	}
+
+	@Override
+	public void loginUser(User u) {
+		// TODO Auto-generated method stub
+		userDao.login(u);
+		
+	}
+
+
 
 }
