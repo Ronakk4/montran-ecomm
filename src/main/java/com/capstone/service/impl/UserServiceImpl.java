d9a7bd06aac2a246009fb5bec86760cb1e1b2df2
unknown <harshshah20200@gmail.com>
2025-08-13 13:57:54 +0530

    add controllers final

--- src/main/java/com/capstone/service/impl/UserServiceImpl.java
+++ src/main/java/com/capstone/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.capstone.dao.UserDao;
 import com.capstone.model.User;
 import com.capstone.service.UserService;
 
@@ -18,42 +17,42 @@ public class UserServiceImpl implements UserService{
 	@Transactional
 	public User findUser(long id) {
 		// TODO Auto-generated method stub
-		return userDao.findUser(id);
+		return userDao.findById(id);
 	}
 
 	@Override
 	@Transactional
-	public void registerUser(User user) {
+	public boolean registerUser(User user) {
 		// TODO Auto-generated method stub
 		 if (user.getEmail() != null && userDao.findByEmail(user.getEmail().trim()) != null) {
-	         //   return false; 
+	            return false; 
 	        }
 	        userDao.save(user);
-	       // return true;
+	        return true;
 		
 	}
-//
-//	@Override
-//	@Transactional
-//	public void updateUser(User user) {
-//		// TODO Auto-generated method stub
-//		 if (userDao.findByEmail(user.getEmail()) != null) {
-//	            userDao.update(user);
-//	        }
-//		
-//	}
 
-//	@Override
-//	@Transactional
-//	public boolean loginUser(User user) {
-//		// TODO Auto-generated method stub
-//		 if (userDao.findByEmail(user.getEmail()) != null) {
-//	            return userDao.login(user);
-//	        }
-//		 return false;
-//			
-//		
-//	}
+	@Override
+	@Transactional
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		 if (userDao.findByEmail(user.getEmail()) != null) {
+	            userDao.update(user);
+	        }
+		
+	}
+
+	@Override
+	@Transactional
+	public boolean loginUser(User user) {
+		// TODO Auto-generated method stub
+		 if (userDao.findByEmail(user.getEmail()) != null) {
+	            return userDao.login(user);
+	        }
+		 return false;
+			
+		
+	}
 
 
 
