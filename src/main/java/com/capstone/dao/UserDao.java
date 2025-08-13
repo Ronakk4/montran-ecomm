9a038cd2de12ba2936ebf64677be771d15a2b06c
Anupkumar Singh <anupsingh4741@gmail.com>
2025-08-13 12:17:58 +0530

    added userdao crud methods

--- src/main/java/com/capstone/dao/UserDao.java
+++ src/main/java/com/capstone/dao/UserDao.java
@@ -1,5 +1,12 @@
 package com.capstone.dao;
 
+import com.capstone.model.User;
+
 public interface UserDao {
-  User findByUserName(String username);
+	User findUser(long id);
+	User findUserByEmail(String email);
+	void saveUser(User u);
+	void updateUser(User u);
+    void deleteUser(User u);	
+	
 }
