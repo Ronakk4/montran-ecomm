30b151d5aeed60a59259ea2b098d11da6172b9ef
Anupkumar Singh <anupsingh4741@gmail.com>
2025-08-13 12:19:44 +0530

    resolved conflict error

--- src/main/java/com/capstone/model/User.java
+++ src/main/java/com/capstone/model/User.java
@@ -1,11 +1,5 @@
 package com.capstone.model;
 
-<<<<<<< HEAD
-public class User {
-	String name;
-=======
->>>>>>> 8b301421f075e6d318b22c1de7f99fe506b86caf
-
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
