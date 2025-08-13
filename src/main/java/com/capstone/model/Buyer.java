61f4695959f16c24e12026985fa949438223b6cb
Bhavya Poddar <poddarbhavya3@gmail.com>
2025-08-13 11:24:39 +0530

    added hibernate.cfg.xml, test seller and buyer, added HibernateUtil

--- src/main/java/com/capstone/model/Buyer.java
+++ src/main/java/com/capstone/model/Buyer.java
@@ -1,5 +1,7 @@
 package com.capstone.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 
 @Entity
@@ -7,4 +9,12 @@ public class Buyer extends User{
 	private String shippingAddress;
 	private String phoneNumber;
 	// orders
+	public Buyer(String name, String email, String password, String role, LocalDateTime createdAt,
+			LocalDateTime updatedAt, String shippingAddress, String phoneNumber) {
+		super(name, email, password, role, createdAt, updatedAt);
+		this.shippingAddress = shippingAddress;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
