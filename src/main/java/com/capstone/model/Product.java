0d188ccacc1cd203a1cfc5ee3c04fad3f6a206c7
Bhavya Poddar <poddarbhavya3@gmail.com>
2025-08-13 12:12:50 +0530

    adding Product class

--- src/main/java/com/capstone/model/Product.java
+++ src/main/java/com/capstone/model/Product.java
@@ -1,5 +1,17 @@
 package com.capstone.model;
 
-public class Product {
+import java.time.LocalDateTime;
 
+public class Product {
+	private String prodId;
+	private String prodName;
+	private String prodDescription;
+	private Double price;
+	private int stockQuantity;
+	private String Category;
+	private LocalDateTime createdAt = LocalDateTime.now();
+	private LocalDateTime updatedAt;
+	
+	
+	
 }
