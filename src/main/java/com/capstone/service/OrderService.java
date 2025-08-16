131f0a2cab9c9132463462768c2dfe365fd8b5e8
Harsh Chheda <chhedaharsh06@gmail.com>
2025-08-16 10:44:54 +0530

    user product order save all crud working

--- src/main/java/com/capstone/service/OrderService.java
+++ src/main/java/com/capstone/service/OrderService.java
@@ -3,12 +3,13 @@ package com.capstone.service;
 
 import java.util.List;
 
+import com.capstone.dto.OrderDTO;
 import com.capstone.model.OrderHeader;
 
 public interface OrderService {
 	List<OrderHeader> getAllOrders(long id);
 	OrderHeader getOrder(long id);
 	void deleteOrder(long id);
-	void saveOrder(OrderHeader o);
+	void saveOrder(OrderDTO o);
 }
 
