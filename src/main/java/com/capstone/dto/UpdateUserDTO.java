c83a5c01158284a3fee345e1d1624a3bc79ec193
Ronakk <ronakkjaveri15@gmail.com>
2025-08-19 18:32:53 +0530

    added change password and update user

--- src/main/java/com/capstone/dto/UpdateUserDTO.java
+++ src/main/java/com/capstone/dto/UpdateUserDTO.java
@@ -1,79 +1,79 @@
-package com.capstone.dto;
-
-public class UpdateUserDTO {
-    private String name;
-    private String email;
-    private String password;
-    private String role;
-
-    // Buyer fields
-    private String shippingAddress;
-    private String phoneNumber;
-
-    // Seller fields
-    private String shopName;
-    private String shopDescription;
-    private String gstNumber;
-	public String getName() {
-		return name;
+	package com.capstone.dto;
+	
+	public class UpdateUserDTO {
+	    private String name;
+	    private String email;
+	    private String password;
+	    private String role;
+	
+	    // Buyer fields
+	    private String shippingAddress;
+	    private String phoneNumber;
+	
+	    // Seller fields
+	    private String shopName;
+	    private String shopDescription;
+	    private String gstNumber;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getRole() {
+			return role;
+		}
+		public void setRole(String role) {
+			this.role = role;
+		}
+		public String getShippingAddress() {
+			return shippingAddress;
+		}
+		public void setShippingAddress(String shippingAddress) {
+			this.shippingAddress = shippingAddress;
+		}
+		public String getPhoneNumber() {
+			return phoneNumber;
+		}
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+		public String getShopName() {
+			return shopName;
+		}
+		public void setShopName(String shopName) {
+			this.shopName = shopName;
+		}
+		public String getShopDescription() {
+			return shopDescription;
+		}
+		public void setShopDescription(String shopDescription) {
+			this.shopDescription = shopDescription;
+		}
+		public String getGstNumber() {
+			return gstNumber;
+		}
+		public void setGstNumber(String gstNumber) {
+			this.gstNumber = gstNumber;
+		}
+	
+	    // getters and setters
+	    
+	    
+	    
+	    
+	    
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getShippingAddress() {
-		return shippingAddress;
-	}
-	public void setShippingAddress(String shippingAddress) {
-		this.shippingAddress = shippingAddress;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getShopName() {
-		return shopName;
-	}
-	public void setShopName(String shopName) {
-		this.shopName = shopName;
-	}
-	public String getShopDescription() {
-		return shopDescription;
-	}
-	public void setShopDescription(String shopDescription) {
-		this.shopDescription = shopDescription;
-	}
-	public String getGstNumber() {
-		return gstNumber;
-	}
-	public void setGstNumber(String gstNumber) {
-		this.gstNumber = gstNumber;
-	}
-
-    // getters and setters
-    
-    
-    
-    
-    
-}
-
+	
